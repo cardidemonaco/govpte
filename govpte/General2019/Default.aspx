9c7c106ffa5e14fa081cf29d46bc903a9e30d97d
Cardi DeMonaco Jr <cardi@rokpte.com>
2019-11-04 13:20:05 -0500

    Updated NuGet packages and created a page for 2019
    Includes my Mayoral race!

---
+++ govpte/General2019/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ElectionDay.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="govpte.General2019" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <p>
+        <a target="_blank" href="http://18.221.153.194/m27/results.html">Official Macomb County Website - Race List</a>
+    </p>
+
+    <asp:GridView ID="gvRaces" runat="server" AutoGenerateColumns="False" OnRowDataBound="gvRaces_RowDataBound">
+        <Columns>
+            <asp:TemplateField HeaderText="Race">
+                <ItemTemplate>
+                    <table>
+                        <tr>
+                            <td colspan="2">
+                                <asp:HyperLink ID="linkRace" runat="server" Font-Size="Larger" style="font-weight: 700">[linkRace]</asp:HyperLink>
+                            </td>
+                        </tr>
+                        <tr>
+                        <tr>
+                            <td>
+                                <asp:Label ID="lblCandidate" runat="server" style="font-weight: 700"></asp:Label>
+                            </td>
+                            <td>
+                                <asp:Label ID="lblCandidateVotes" runat="server"></asp:Label>
+                            </td>
+                        </tr>
+                    </table>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+
+</asp:Content>
---
+++ govpte/General2019/Default.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+using HtmlAgilityPack;
+
+namespace govpte
+{
+    public partial class General2019 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Override pages to use on election site
+            //  Eastpointe Council election is ranked-choice voting, so it is not listed
+            List<string> urls = new List<string>
+            {
+                "http://18.221.153.194/m27/5.html", //Eastpointe Mayor
+                "http://18.221.153.194/m27/51.html", //Eastpointe Charter Amendment
+                "http://18.221.153.194/m27/52.html", //Eastpointe Community Schools Sinking Fund 
+                "http://18.221.153.194/m27/16.html", //Mt. Clemens Mayor
+                "http://18.221.153.194/m27/17.html", //Mt. Clemens City Commission
+                "http://18.221.153.194/m27/21.html", //Roseville City Council
+                "http://18.221.153.194/m27/23.html", //St. Clair Shores Council
+                "http://18.221.153.194/m27/25.html", //Sterling Heights Council
+                "http://18.221.153.194/m27/28.html", //Warren Mayor
+                "http://18.221.153.194/m27/29.html", //Warren Clerk
+                "http://18.221.153.194/m27/30.html", //Warren Treasurer
+                "http://18.221.153.194/m27/31.html", //Warren At-Large Council
+                "http://18.221.153.194/m27/32.html", //Warren District 1
+                "http://18.221.153.194/m27/34.html", //Warren District 3
+                "http://18.221.153.194/m27/35.html" //Warren District 4
+            };
+
+            gvRaces.DataSource = urls;
+            gvRaces.DataBind();
+        }
+
+        protected void gvRaces_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                string drv = (string)e.Row.DataItem;
+
+                HyperLink linkRace = (HyperLink)e.Row.FindControl("linkRace");
+                Label lblBarGraph = (Label)e.Row.FindControl("lblBarGraph");
+                Label lblCandidate = (Label)e.Row.FindControl("lblCandidate");
+                Label lblCandidateVotes = (Label)e.Row.FindControl("lblCandidateVotes");
+
+                var url = drv;
+                var web = new HtmlWeb();
+                var doc = web.Load(url);
+
+                HtmlNode hncRace = doc.DocumentNode.SelectSingleNode("//*[@class=\"racetitle1\"]");
+                HtmlNodeCollection hncRaces = doc.DocumentNode.SelectNodes("//*[@class=\"racetable\"]");
+                int i = 2;
+                foreach (HtmlNode hnRace in hncRaces)
+                {
+                    linkRace.Text = hncRace.InnerText;
+                    linkRace.NavigateUrl = drv;
+                    linkRace.Target = "_blank";
+
+                    IEnumerable<HtmlNode> hncCandidates = hnRace.SelectNodes($"/html[1]/body[1]/center[2]/table[{i}]//*[@class=\"candname\"]");
+                    foreach (HtmlNode hnCandidate in hncCandidates)
+                    {
+                        lblCandidate.Text += hnCandidate.InnerText + "<br />";
+                    }
+
+                    HtmlNodeCollection hncCandidatesVoteTotals = hnRace.SelectNodes($"/html[1]/body[1]/center[2]/table[{i}]//*[@class=\"vtotal\"]");
+                    int sum = 0;
+                    foreach (HtmlNode hnCandidateVoteTotal in hncCandidatesVoteTotals)
+                    {
+                        lblCandidateVotes.Text += hnCandidateVoteTotal.InnerText + "<br />";
+                        sum += Convert.ToInt32(hnCandidateVoteTotal.InnerText.Replace(",", ""));
+                    }
+
+                    lblCandidate.Text += "<br />";
+                    lblCandidateVotes.Text += "<br />";
+
+                    i++;
+                }
+            }
+        }
+    }
+}
